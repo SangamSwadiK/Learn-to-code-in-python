1a344872d86c3122da12282fcfd3315a42a79e01
SangamSwadiK <sangamswadi.k18@gmail.com>
2020-08-28 08:21:55 +0530

    completed first 5 sections of the course

--- 06-Strings-The-Basics/string-indexing-with-negative-values.py
+++
@@ -1,8 +0,0 @@
-# The square brackets can also take negative values
-
-topic = "Best city"
-print( topic[ -1 ] )
-print( topic[ -2 ] )
-
-# Index error: string index is out of range error
-#print( topic[ -100 ] )
