a3482d033930d7896e8cd31c5453ff92bd91d7a9
SangamSwadiK <sangamswadi.k18@gmail.com>
2020-08-30 17:05:38 +0530

    removed control flow section

--- 08-Control-Flow/the-or-keyword.py
+++
@@ -1,7 +0,0 @@
-# Atleast one of the conditions is True
-if 5 - 8 or 7 -11:
-    print('Atleast one of the conditions is True')
-
-if "cat" == 'cat' or 'dog' == 'donkey':
-    print('Atleast one of the conditions is True!')
-    
